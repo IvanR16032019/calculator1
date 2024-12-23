2f74856ee30a077c1e85f9817d3940e7482c5545
Ivan Rodriguez <Irodriguez@DESKTOP-J1JP2L6>
2024-12-23 09:44:32 -0500

    alla voy

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import Calculator from './componentes/Calculator';
+import './App.css'; // Asegúrate de que este archivo esté importado
+
+function App() {
+  useEffect(() => {
+    // Esta función se ejecutará una vez al cargar la página.
+    const logo = document.getElementById('logo');
+    logo.classList.add('logo-animation'); // Añadimos la clase para iniciar la animación
+  }, []);
+
+  return (
+    <div>
+      <div className="header">
+        <img
+          id="logo"
+          src="../public/imagenes/ircdev.png" // Reemplaza con la URL de tu logo
+          alt="Logo de la empresa"
+          className="logo"
+        />
+      </div>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Calculator.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import '../hojas-de-estilo/Calculator.css';
+
+const Calculator = () => {
+  const [input, setInput] = useState('');
+  const [error, setError] = useState('');
+
+  const handleClick = (value) => {
+    setInput(input + value);
+    setError(''); // Limpiar el mensaje de error al ingresar nuevos valores
+  };
+
+  const handleClear = () => {
+    setInput('');
+    setError('');
+  };
+
+  const handleCalculate = () => {
+    if (input === '') {
+      setError('Inserta los valores');
+      return;
+    }
+
+    // Verificar si solo hay operadores
+    const operators = /[+\-*/]/;
+    if (operators.test(input) && !/[0-9]/.test(input)) {
+      setError('Inserta valores adecuados');
+      return;
+    }
+
+    try {
+      // Evaluar la expresión matemática
+      setInput(eval(input).toString());
+    } catch (e) {
+      setError('Error');
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <div className="calculator-display">
+        <input type="text" value={input} readOnly />
+        {error && <p className="error-message">{error}</p>}
+      </div>
+      <div className="calculator-buttons">
+        <button onClick={() => handleClear()} className="button clear">
+          Empty
+        </button>
+        <button onClick={() => handleClick('/')} className="button operator">
+          /
+        </button>
+        <button onClick={() => handleClick('*')} className="button operator">
+          *
+        </button>
+        <button onClick={() => handleClick('-')} className="button operator">
+          -
+        </button>
+
+        <button onClick={() => handleClick('7')} className="button">
+          7
+        </button>
+        <button onClick={() => handleClick('8')} className="button">
+          8
+        </button>
+        <button onClick={() => handleClick('9')} className="button">
+          9
+        </button>
+        <button onClick={() => handleClick('+')} className="button operator">
+          +
+        </button>
+
+        <button onClick={() => handleClick('4')} className="button">
+          4
+        </button>
+        <button onClick={() => handleClick('5')} className="button">
+          5
+        </button>
+        <button onClick={() => handleClick('6')} className="button">
+          6
+        </button>
+
+        <button onClick={() => handleClick('1')} className="button">
+          1
+        </button>
+        <button onClick={() => handleClick('2')} className="button">
+          2
+        </button>
+        <button onClick={() => handleClick('3')} className="button">
+          3
+        </button>
+        <button onClick={() => handleClick('0')} className="button">
+          0
+        </button>
+
+        <button onClick={() => handleClick('.')} className="button">
+          .
+        </button>
+        <button onClick={handleCalculate} className="button equal">
+          =
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
